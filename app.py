c288d287318f6e0bddd90dcba553d6f16163790c
Ian Nebres <airforceuuser@gmail.com>
2026-04-03 12:46:41 -0400

    added new feature

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_socketio import SocketIO, emit, join_room
 import random
 import string
 import os
 import logging
+
+games = {}
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s")
 
@@ -40,9 +42,10 @@ def game_room(code):
 @socketio.on("create_game")
 def create_game():
     code = generate_code()
-    # Create game with Host
-    games[code] = ["Host"]
-    print(f"Game created: {code}")
+    games[code] = {
+        "teams": [],
+        "players": {}
+    }
     emit("redirect", code)
 
 @socketio.on("join_game")
@@ -58,12 +61,43 @@ def join_game(data):
         emit("error", "Game code not found!")
         return
 
-    if team not in games[code]:
-        games[code].append(team)
+    game = games[code]
+    sid = request.sid  # unique socket ID
+
+    # Prevent duplicate teams
+    if team not in game["teams"]:
+        game["teams"].append(team)
+
+    # Track player
+    game["players"][sid] = team
 
     join_room(code)
-    emit("update_teams", games[code], room=code)
+    emit("update_teams", game["teams"], room=code)
+
     print(f"{team} joined game {code}")
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    sid = request.sid
+
+    for code, game in list(games.items()):
+        if sid in game["players"]:
+            team = game["players"].pop(sid)
+
+            # Remove team if no one else is using it
+            if team not in game["players"].values():
+                if team in game["teams"]:
+                    game["teams"].remove(team)
+
+            emit("update_teams", game["teams"], room=code)
+            print(f"{team} disconnected from {code}")
+
+            # Delete empty game
+            if not game["players"]:
+                del games[code]
+                print(f"Game {code} deleted (empty)")
+
+            break
 
 # -----------------------------
 # Run the app
