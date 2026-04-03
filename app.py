a32fb9b0f9a547b93452b92900d89a130689110a
Ian Nebres <airforceuuser@gmail.com>
2026-04-03 12:34:58 -0400

    added new feature

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def join_game(data):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # use Railway port
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
